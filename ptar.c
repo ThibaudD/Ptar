fe4d6e056be3f32700f1baee80793fdb85ffa5fe
ThibaudD <thibaud.de@gmail.com>
2014-01-25 13:58:12 +0100

    update

--- ptar.c
+++ ptar.c
@@ -7,7 +7,20 @@
 #include <string.h>
 #include <pthread.h>
 
-#define NUM_THREADS 3
+#define NUM_THREADS 8
+
+static pthread_mutex_t verrou_creation = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t dispo = PTHREAD_MUTEX_INITIALIZER;;
+static int* thread_dispo;
+static int nb_dispo;
+
+unsigned int qflag = 0;
+unsigned int vflag = 0;
+unsigned int lflag = 0;
+unsigned int aflag = 0;
+unsigned int dflag = 0;
+unsigned int nbT = 0;
+char* dossierExtraire;
 
 struct header_posix_ustar {
 	char name[100];
@@ -35,6 +48,7 @@ struct dataThread{
 	int fd;
 	struct header_posix_ustar files_info;
 	char* name;
+	int num;
 };
 
 int octal_to_dec(char *octalChar){
@@ -56,21 +70,12 @@ int octal_to_dec(char *octalChar){
 	return dec;
 }
 
-void *travail(void *null){
-	int i;
-	double result=0.0;
-	for(i=0;i<10000000;i++){
-		result=result+(double)random();
-		printf("Resultat = %e\n",result);
-		pthread_exit((void *) 0);
-	}
-}
-
 void lister(int fd, struct header_posix_ustar files_info, char* archive){
 	int size = 0;
 	int lecture = read(fd,&files_info,512);
-	printf("List of directories/files in the archive: \"%s\" :\n", archive);
-	printf("%s\n",files_info.name);
+	size = octal_to_dec(files_info.size);
+	if(!qflag)printf("List of directories/file in the archive \"%s\" :\n", archive);
+	printf("-%s\n",files_info.name);
 	while(lecture != 0){
 		size = octal_to_dec(files_info.size);
 		if(size!=0){
@@ -78,36 +83,47 @@ void lister(int fd, struct header_posix_ustar files_info, char* archive){
 		}
 		lecture = read(fd,&files_info,512);
 		if(strlen(files_info.name)!=0){
-			printf("%s\n",files_info.name);
+			printf("-%s\n",files_info.name);
 		}
 	}
 }
 
 void extraire_dossier(char* archive, char* chemin){
 
-	char *d = malloc(FILENAME_MAX*sizeof(char));
-	if((d = strrchr(archive, '/'))!=NULL){
-		strncat(chemin,d,strlen(d)-4);
-	}
-	else {
-		char slash[FILENAME_MAX]="/";
-		strcat(slash,archive);
-		strncat(chemin,slash,strlen(slash)-4);
-	}
-	printf("path : %s\n",chemin);
-	if(chdir(chemin)!=0){
-		if(mkdir(chemin,00700) == 0){
-			char *arch_dossier = malloc(FILENAME_MAX*sizeof(char));
-			strcpy(arch_dossier,archive);
-			arch_dossier[strlen(arch_dossier)-4] = '\0';
-			if(d != NULL){
-				d[strlen(d)-4] = '\0';
-				printf("creation of the directory %s\n",d+1);
+	if(dflag == 2){
+				char slash[FILENAME_MAX]="/";
+				strcat(chemin,slash);
+				strcat(chemin,dossierExtraire);
+				if(chdir(chemin)!=0){
+					if(mkdir(chemin,00700)==0){
+						if(!qflag) printf("directory creation %s\n",dossierExtraire);
+						chdir(chemin);
+					}
+				}
+	}else{
+		char *d = malloc(FILENAME_MAX*sizeof(char));
+		char *archive_dossier = malloc(FILENAME_MAX*sizeof(char));
+		strcpy(archive_dossier,archive);
+		if((d = strrchr(archive_dossier, '/'))!=NULL){
+				strncat(chemin,d,strlen(d)-4);
+		}
+		else {
+				char slash[FILENAME_MAX]="/";
+				strcat(slash,archive_dossier);
+				strncat(chemin,slash,strlen(slash)-4);
+		}
+		if(chdir(chemin)!=0){
+			if(mkdir(chemin,00700) == 0){
+				archive_dossier[strlen(archive_dossier)-4] = '\0';
+				if(d != NULL){
+					d[strlen(d)-4] = '\0';
+					if(!qflag)printf("directory creation %s\n",d+1);
+				}
+				else {if(!qflag) printf("directory creation %s\n",archive_dossier);}
+				chdir(chemin);
+				//free(arch_dossier);
 			}
-			else printf("creation of the directory %s\n",arch_dossier);
-			chdir(chemin);
-			//free(d);
-			//free(arch_dossier);
+
 		}
 	}
 	//free(d);
@@ -116,13 +132,15 @@ void extraire_dossier(char* archive, char* chemin){
 void *creation_fichier(void *arg){
 	struct dataThread *mine = malloc(sizeof(struct dataThread));
 	mine = (struct dataThread *)arg;
-	//printf("nom_fichier_dansThread : **%s**\n", mine->name);
 
-	printf("creation file %s\n",mine->name);
+	if(!qflag && vflag)printf("Thread n°%d -> file creation %s\n",(mine->num+1),mine->name);
+	else if(!qflag && !vflag)printf("file creation %s\n",mine->name);
 	int fd_file = creat(mine->name,S_IRWXU);
 	int fin = mine->size%512;
 	int nb_bloc = mine->size/512;
 	int compt_bloc = 0;
+	thread_dispo[mine->num] = 0;
+	pthread_mutex_unlock(&verrou_creation);
 
 	if(mine->size > 512){
 		for(compt_bloc=0;compt_bloc<nb_bloc;compt_bloc++){
@@ -138,94 +156,81 @@ void *creation_fichier(void *arg){
 		write(fd_file,mine->files_info.name,mine->size);
 		fsync(fd_file);
 	}
+	if(!qflag && vflag)printf("Thread n°%d end\n",(mine->num+1));
 	close(fd_file);
-
-	pthread_exit (0);
-	//*/
+	thread_dispo[mine->num] = 1;
+	nb_dispo++;
+	pthread_mutex_unlock(&dispo);
+	pthread_exit((void *) 0);
 }
 
-/*void creation_fichier(int lecture, int size, int fd, struct header_posix_ustar files_info,char* name){
-
-	printf("creation fichier %s\n",name);
-	int fd_file = creat(name,S_IRWXU);
-	int fin = size%512;
-	int nb_bloc = size/512;
-	int compt_bloc = 0;
-
-	if(size > 512){
-		for(compt_bloc=0;compt_bloc<nb_bloc;compt_bloc++){
-			lecture = read(fd,&files_info,512);
-			write(fd_file,files_info.name,512);
-			fsync(fd_file);
-		}
-		lecture = read(fd,&files_info,512);
-		write(fd_file,files_info.name,fin);
-		fsync(fd_file);
-	} else {
-		lecture = read(fd,&files_info,512);
-		write(fd_file,files_info.name,size);
-		fsync(fd_file);
-	}
-	close(fd_file);
-}
-*/
 void extraire(int fd, struct header_posix_ustar files_info, char* archive, char* arg){
 	int lecture = 1;
 	int size = 0;
 	char *name;
+	int creation_dossier = 0;
 	char chemin[FILENAME_MAX];
 	getcwd(chemin,sizeof(chemin));
-	if(!strcmp(arg,"-xd")){
+	if(dflag > 0){
+		creation_dossier = 1;
 		extraire_dossier(archive,chemin);
 	}
-
-
+	if(nbT==0){
+		nbT=NUM_THREADS;
+	}
 	char *directory = malloc(FILENAME_MAX*sizeof(char));
 	char *dossier = malloc(FILENAME_MAX*sizeof(char));
 	char *i;
-	int position = 0;
+	int position_courante = 0;
 
-	/* Threads
-			int rc, t, status;
-			pthread_t thread[3];
-			pthread_attr_t attr;
+	pthread_t thread[nbT];
+	int fds[nbT];
 
-			pthread_attr_init(&attr);
-			pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	if(creation_dossier == 1){
+		chdir("..");
+	}
+	int index_thread;
+	for(index_thread=0;index_thread<nbT;index_thread++){
+		thread_dispo[index_thread] = 1;
+		fds[index_thread] = open(archive,O_RDONLY); //ouverture de l'archive
+		if(fds[index_thread]==-1){
+			if(!qflag)perror("Error fd for the thread");
+			exit(0);
+		}
+	}
+	if(creation_dossier == 1){
+		chdir(chemin);
+	}
 
-			for(t=0; t<NUM_THREADS;t++){
-				printf("Creation thread %d\n",t);
-				if((rc = pthread_create(&thread[t], &attr, travail, NULL))){
-					printf("Erreur de _create() : %d\n", rc);
-					exit(-1);
-				}
-			}
-			pthread_attr_destroy(&attr);
-			for(t=0; t<NUM_THREADS; t++){
-				if(rc = pthread_join(thread[t], (void**)&status)){
-					printf("ERREUR de _join() : %d\n", rc);
-					exit(-1);
-				}
-				printf("Rejoint thread %d. (ret=%d)\n", t, status);
-			}
-			pthread_exit(NULL);
-	 */
-
+	int num_thread = 0;
 	while(lecture!=0){
-		lecture = read(fd,&files_info,512);
-		position +=512;
+		pthread_mutex_lock(&verrou_creation); // qui se débloque dans la procédure creation_fichier
+		if(nb_dispo == 0 || nb_dispo ==1) // s'il ne reste qu'un thread dispo le mutex sera bloqué pour la prochaine itération
+			pthread_mutex_lock(&dispo);	// se débloque à la fin de la procédure creation_fichier
+		int i_dispo;
+		for(i_dispo = 0; i_dispo<nbT; i_dispo++){ // boucle pour récupérer un thread dispo
+			if(thread_dispo[i_dispo] == 1){
+				num_thread = i_dispo;
+				if(!qflag && vflag)printf("Thread n°%d available\n",num_thread+1);
+				break;
+			}
+			else if(!qflag && vflag)printf("Thread n°%d not available\n",i_dispo+1);
+		}
+		lecture = read(fd,&files_info,512); // on lit l'entete
 		size = octal_to_dec(files_info.size);
 		name = files_info.name;
 		if(strlen(name) != 0){
 			// il s'agit d'un dossier
 			if(size == 0){
+				pthread_mutex_unlock(&verrou_creation);  // comme c'est un dossier on débloque les mutex
+				pthread_mutex_unlock(&dispo);
 				name[strlen(name)-1] = '\0';
 				while((i = strchr(name, '/'))!=NULL){
 					int decal = i-name;
 					dossier = strndup(name, decal);
 					if(chdir(dossier)!=0){
 						if(mkdir(dossier,00700) == 0){
-							printf("create directory %s\n",name);
+							if(!qflag)printf("directory creation %s\n",name);
 							chdir(dossier);
 						}
 					}
@@ -234,7 +239,7 @@ void extraire(int fd, struct header_posix_ustar files_info, char* archive, char*
 				}
 				if(chdir(dossier)!=0){
 					if(mkdir(name,00700)== 0){
-						printf("create directory %s\n",name);
+						if(!qflag)printf("directory creation  %s\n",name);
 					}
 				}
 				chdir(chemin);
@@ -248,7 +253,7 @@ void extraire(int fd, struct header_posix_ustar files_info, char* archive, char*
 					dossier = strndup(directory, decal);
 					if(chdir(dossier)!=0){
 						if(mkdir(dossier,00700) == 0){
-							//printf("creation dossier %s\n",dossier);
+							if(!qflag)printf("directory creation %s\n",dossier);
 							chdir(dossier);
 						}
 					}
@@ -257,54 +262,117 @@ void extraire(int fd, struct header_posix_ustar files_info, char* archive, char*
 				}
 				chdir(chemin);
 
-				int rc, t, status;
-				pthread_t th1;
-				int fd_1 = open(archive,O_RDONLY); //ouverture de l'archive
-				lseek(fd_1,position,SEEK_SET);// on place le curseur au début des données (marche pas)
-				lseek(fd,(size/512+1)*512,SEEK_CUR); // on déplace le descripteur courant vers la prochaine entête
-				position +=(size/512+1)*512;
+				nb_dispo--;                    //un thread va être utilisé
+				thread_dispo[num_thread] = 0;  // on change la disponibilité du thread quiv a être utilisé
+				position_courante = lseek(fd,0,SEEK_CUR);	// debut du fichier courant
 
-				struct dataThread data;
-				data.lecture = lecture;
-				data.size = size;
-				data.fd = fd_1;
-				data.files_info = files_info;
-				data.name = name;
-				//printf("nom_fichier_avantThread : **%s**\n", data.name);
-				pthread_create (&th1, NULL, creation_fichier, (void *)&data);
-				if(rc = pthread_join(th1, (void**)&status)){ // attend la fin du thread
-					printf("ERREUR de _join() : %d\n", rc);
-					exit(-1);
-				}
-				printf("Rejoint thread %d. (ret=%d)\n", t, status);
-				//creation_fichier(lecture, size, fd, files_info, name);*/
+				lseek(fds[num_thread],position_courante,SEEK_SET);// on place le curseur au début des données du fichier courant
+				lseek(fd,(size/512+1)*512,SEEK_CUR);	// on déplace le descripteur courant vers la prochaine entête
+				struct dataThread *data = malloc(sizeof(struct dataThread));
+				data->lecture = lecture;
+				data->size = size;
+				data->fd = fds[num_thread];
+				data->files_info = files_info;
+				data->name = name;
+				data->num = num_thread;
+
+				if(pthread_create (&thread[num_thread], NULL, creation_fichier, (void *)data) !=0)
+					if(!qflag)printf("Error thread");
 			}
+		} else {
+			lecture = 0;
 		}
 	}
-	pthread_exit(NULL);
+	int t,rc,status;
+	for(t=0;t<nbT;t++){
+		if(thread_dispo[t] == 0 && (rc = pthread_join(thread[t],(void **)&status))){
+			if(!qflag)printf("ERROR  join() : %d\n", rc);
+			exit(-1);
+		}
+		if(!qflag && vflag)printf("Join thread %d\n",t+1,status);
+	}
+	//free(dossier);
+	//free(directory);
+	for(t=0;t<nbT;t++){
+		close(fds[t]);
+	}
+	if(!qflag)printf("Extraction end !\n");
 }
 
 int main(int argc, char *argv[]){
-	if(argc < 3){
-		printf("ptar: you must specify one of the options:\n -l list files in the archive \n -x extract files in the current directory \n -xd extract files in a directory with the name of the tar file\n");
-		return 0;
-	}
-	char *archive = argv[2];
+if(argc<2 ){
+	printf("ptar need one or further arguments \n You can see help with the option -h\n");
+	return 1;
+}
+	int option;
+	char* nbThreads;
+	while ((option = getopt (argc, argv, "d:qvlt:h")) != -1){
+		switch (option)
+		{
+		case 'h':
+			printf("These options are available:\n -q quiet\n -v verbose\n -l list files in the archive\n -d [directory] extract in a directory \n -t nb_threads define the number of threads\n -h help\n");
+		case 'q':
+			qflag = 1;
+			break;
+		case 'v':
+			vflag = 1;
+			break;
+		case 'l':
+			lflag = 1;
+			break;
+		case 'd':
+			dflag = 1;
+			if (optarg[0] != '-' && optarg[strlen(optarg)-4] != '.'){
+				dflag = 2;
+				dossierExtraire = malloc(sizeof(char)*strlen(optarg));
+				dossierExtraire = optarg;
+			} else {
+				dossierExtraire = "";
+			}
 
+			break;
+		case 't':
+			if (optarg[0] != '-' && optarg[strlen(optarg)-4] != '.'){
+				nbT = atoi(optarg);
+			} else {
+				printf("the option -t need an integer\n");
+				return 1;
+			}
+			break;
+		default:
+			printf("Wrong argument\n");
+			return 1;
+		}
+	}
+	if(nbT >0){
+		thread_dispo = malloc(sizeof(int)*nbT);
+		thread_dispo[nbT];
+		nb_dispo = nbT;
+	} else {
+		thread_dispo = malloc(sizeof(int)*NUM_THREADS);
+		nb_dispo = NUM_THREADS;
+	}
+	char *archive = argv[argc-1];
+	if(strcmp(&archive[strlen(archive)-4],".tar") && strcmp(&archive[strlen(archive)-2],"-h")){
+		printf("error, your file is not an tar archive\n");
+		exit (1);
+	}
 	int fd=open(archive,O_RDONLY);
 	if(fd==-1){
-		printf("Archive not found\n");
-		exit(0);
+		if(!qflag) perror("Error Archive ");
+		exit(1);
 	}
 	struct header_posix_ustar files_info;
 
-	/* Lister */
-	if(!strcmp(argv[1],"-l")){
+	//Lister
+	if(lflag){
 		lister(fd,files_info,archive);
-		/* Extraire */
-	} else if(!strcmp(argv[1],"-x") || !strcmp(argv[1],"-xd")){
+	//Extraire
+	} else {
 		extraire(fd,files_info,archive,argv[1]);
 	}
 	close(fd);
+	//if(dflag==2)free(dossierExtraire);
+	//free(thread_dispo);
 	return 0;
 }
